73aaeb83def3706b8b30ccc599cfb2d1f9a31019
test <git@gitlab.com:pastellest/test.git>
2021-05-22 14:19:11 +0200

    3

--- src/Blatt04/test.java
+++ src/Blatt04/test.java
@@ -5,6 +5,8 @@ public class test {
     public static void main(String[] args) {
         Point point = new Point(1, 1);
         System.out.println(point);
+
+        System.out.println("aaa");
     }
 
 }
